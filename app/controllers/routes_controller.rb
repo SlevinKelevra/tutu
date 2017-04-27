e902c1743977791564da51d9a82db92a2b376972
Your Name <you@example.com>
2017-04-27 22:26:45 +0300

    Fix

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -4,8 +4,7 @@ class RoutesController < ApplicationController
     @routes = Route.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @route = Route.new
@@ -21,8 +20,7 @@ class RoutesController < ApplicationController
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :passenger
   belongs_to :train
   belongs_to :first_station, class_name: 'RailwayStation'
-  belongs_to :last_station, class_name: "RailwayStation"
+  belongs_to :last_station, class_name: 'RailwayStation'
 end
