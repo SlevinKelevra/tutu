364b7039f955f28b5cfaf34ee8c47f883673ee8c
Your Name <you@example.com>
2017-05-05 13:03:42 +0300

    Added sorting station in the itinerary

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,5 +2,8 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :trains
+
   validates :title, presence: true
+
+  scope :sort, -> { order('railway_stations_routes.station_number') }
 end
