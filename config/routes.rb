c12df009b69ed342a6d913f0ea85f55fafcdad5f
Your Name <you@example.com>
2017-05-10 02:20:23 +0300

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :passengers
+  devise_for :passengers, :controllers => { registrations: 'registrations' }
   get 'home_page/index'
 
   root 'home_page#index'
