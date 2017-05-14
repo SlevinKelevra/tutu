96715504f3887f92a073655a97727c53a9792abb
SlevinKelevra <poztalion2@yandex.ru>
2017-05-14 20:14:30 +0300

    Add mail

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,21 @@ class Ticket < ApplicationRecord
   belongs_to :train
   belongs_to :first_station, class_name: 'RailwayStation', foreign_key: :first_station_id
   belongs_to :last_station, class_name: 'RailwayStation', foreign_key: :last_station_id
+
+  after_create :send_buy_notification
+  after_destroy :send_cancel_notification
+
+  def route_name
+    "#{first_station.title} - #{last_station.title}"
+  end
+
+  private
+
+  def send_buy_notification
+    TicketsMailer.buy_ticket(self.passenger, self).deliver_now
+  end
+
+  def send_cancel_notification
+    TicketsMailer.cancel_ticket(self.passenger, self).deliver_now
+  end
 end
